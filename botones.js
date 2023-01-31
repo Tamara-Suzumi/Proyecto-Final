865bcc431eeb2a4c85511eec75acedf8946e53e0
Tamara-Suzumi <124094369+Tamara-Suzumi@users.noreply.github.com>
2023-01-31 19:55:17 -0300

    Add files via upload

---
+++ botones.js
@@ -0,0 +1,7 @@
+
+//Empezando//
+
+document.getElementById('boton').addEventListener('click', function() {
+    
+    document.getElementById("demo").innerHTML = "https://www.youtube.com/watch?v=K4xLi8IF1FM";
+});
